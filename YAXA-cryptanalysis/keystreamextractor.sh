7e16758f15ab836f23fbd1ae073a7e4b1a5afa8d
kenny <kenny@AMD>
2018-12-29 07:12:26 -0800

    Removed non tar files

--- YAXA-cryptanalysis/keystreamextractor.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-rm ./mockfile
-./mockfilegenerator.sh ./mockfile $1
-./yaxafileutil -e ./mockfile ./mockfile.enc
-./stripsalt ./mockfile.enc ./mockfile.enc.unsalted 24
-./pxorc ./mockfile ./mockfile.enc.unsalted > ./mockfile.keystream
-./pxorc ./mockfile.enc.unsalted ./mockfile.keystream > ./mockfile.denc.fromunsalted
-md5sum -b ./mockfile ./mockfile.denc.fromunsalted
--- YAXA-cryptanalysis/makedummydatabase.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-rm ./mockfile
-./mockfilegenerator.sh ./mockfile $1
-./yaxafileutil -e ./mockfile ./mockfile.enc pass
