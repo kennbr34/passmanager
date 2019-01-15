2d7a7ed643e946ea86b995ce199097f23ebf0a03
kenny <kenny@AMD>
2019-01-14 17:22:07 -0800

    updating changelog and version number

--- src/passmanager.c
+++ src/passmanager.c
@@ -3272,7 +3272,7 @@ int printSyntax(char* arg)
 \n     \t-x 'database password' (the current database password to decrypt/with) \
 \n     \t-c 'first-cipher:second-cipher' - Update algorithms in cascade\
 \n     \t-H 'first-digest:second-digest' - Update digests used for cascaded algorithms' KDFs\
-\nVersion 2.1.3\
+\nVersion 2.1.4\
 \n\
 ",
         arg);
